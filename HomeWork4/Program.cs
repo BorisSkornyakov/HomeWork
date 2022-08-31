26e5961b795c6062ef2642827a45329b4008a8c3
BorisSkornyakov <3601124@gmail.com>
2022-08-31 10:53:00 +0500

    Задача 4 готова

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Принимаем число N и выводим все четные числа в диапазоне от 1 до N
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 2; i <= number; i= i+2)
+{
+    Console.Write(i + ", ");
+}
