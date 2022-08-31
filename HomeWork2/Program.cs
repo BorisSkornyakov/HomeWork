78f587a36fac2264a60c2f8a018d2c61630946d7
BorisSkornyakov <3601124@gmail.com>
2022-08-31 09:51:01 +0500

    задача 2 готова

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Водим три числа и выводим наибольшее из трех
+int[] numbers = new int[3];                             // Иничиализируем массив из 3-х элементов
+Console.Write("Введите первое число ");                 // записываем в массив по очереди 3 значения
+numbers[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+numbers[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+numbers[2] = Convert.ToInt32(Console.ReadLine());
+int max = numbers[0];                                   //Инициализируем переменную max
+for (int i = 0; i < 3; i++)                             // Находим максимальное значение массива и записываем его в переменную max
+{                             
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+}
+
+Console.WriteLine(max);                                 // Выводим в консоль максимальное значение
