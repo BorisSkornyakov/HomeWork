f4cae4588a70876ab874a8214597d12c52953a51
BorisSkornyakov <3601124@gmail.com>
2022-08-31 10:22:43 +0500

    Задача 3 готова

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определяем является ли введенное число четным
+Console.Write("Введите число ");                       // запрашиваем ввод числа
+int number = Convert.ToInt32(Console.ReadLine());      // Считываем число
+int remains = number % 2;                              //Находим остаток при делении на 2
+if(remains > 0)                                        // Если остаток больше 0 - число не четное
+{
+Console.WriteLine("Число " + number + " не четное");
+}
+else
+{
+    Console.WriteLine("Число " + number + " четное");  
+}
+
+                                
